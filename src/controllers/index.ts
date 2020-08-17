ad236855622c306361fbc89250b14f9040b5bcb5
Wei Jie <lancerace@hotmail.com>
2020-08-17 15:24:55 +0800

    syntax error

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -7,7 +7,7 @@ const lineConfig = config[process.env.ENVIRONMENT];
 console.log(lineConfig.line);
 router.post("/webhook", line.middleware(lineConfig.line), async (req: Request, res: Response) => {
     console.log("webhook");
-    console.log("body.event:",req.body.events.);
+    console.log("body.event:",req.body.events);
     Promise
     .all(req.body.events.map(event=>{console.log("event",event)}))
     .then(result=> res.json({ message: "webhook",result:result }));
