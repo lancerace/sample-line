5ab876d0b13a4e525093e313736db9cd1ef92b11
Wei Jie <lancerace@hotmail.com>
2020-08-17 15:40:54 +0800

    update webhook

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -7,16 +7,22 @@ const lineConfig = config[process.env.ENVIRONMENT];
 console.log(lineConfig.line);
 router.post("/webhook", line.middleware(lineConfig.line), async (req: Request, res: Response) => {
     console.log("webhook");
-    console.log("body.event:",req.body.events);
-    Promise
-    .all(req.body.events.map(event=>{
-        console.log('type:',event.type);
-        console.log('messasge type',event.message.type);
+    console.log("body.event:", req.body.events);
+
+    const client = new line.Client(lineConfig.line);
+    Promise.all(req.body.events.map(event => {
+        console.log('type:', event.type);
+        console.log('messasge type', event.message.type);
         if (event.type !== 'message' || event.message.type !== 'text') {
             return Promise.resolve(null);
-          }
+        }
+
+        return client.replyMessage(event.replyToken, {
+            type: 'text',
+            text: event.message.text
+        });
     }))
-    .then(result=> res.json({ message: "webhook",result:result }));
+        .then(result => res.json({ message: "webhook", result: result }));
 
 
 
