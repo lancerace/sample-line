2a35ecfc7b2fd917ccd8a78f8b3a2e4b5770081f
Wei Jie <lancerace@hotmail.com>
2020-08-17 13:29:46 +0800

    added test response for webbook

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,10 +1,12 @@
-import express, { Request, Response } from "express";
+import express, { Request, Response, json } from "express";
 const router: express.Router = express.Router();
 
 
-router.get("/webhook", async (request: Request,response:Response)=>{
+router.get("/webhook", async (request: Request, response: Response) => {
 
     console.log("webhook");
     console.log(response);
+
+    return response.json({ message: "webhook" })
 });
 export default router;
