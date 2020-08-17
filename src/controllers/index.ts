afad5473e840b36750e00505d9d216d343669263
Wei Jie <lancerace@hotmail.com>
2020-08-17 13:34:20 +0800

    remove log

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -5,7 +5,7 @@ const router: express.Router = express.Router();
 router.get("/webhook", async (request: Request, response: Response) => {
 
     console.log("webhook");
-    console.log(response);
+    //console.log(response);
 
     return response.json({ message: "webhook" })
 });
