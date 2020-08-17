e445977ec6a90cd74c26691e064c075a40c1535c
Wei Jie <lancerace@hotmail.com>
2020-08-17 16:30:57 +0800

    log,trace no signature err

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -5,9 +5,14 @@ const router: express.Router = express.Router();
 
 const lineConfig = config[process.env.ENVIRONMENT];
 console.log(lineConfig.line);
-router.post("/webhook", line.middleware(lineConfig.line), async (req: Request, res: Response) => {
+router.post("/webhook", line.middleware(lineConfig.line), async (req, res) => {
     console.log("webhook");
     console.log("body.event:", req.body.events);
+console.log(req.header);
+console.log(req.headers);
+console.log(req.headers["x-line-signature"]);
+
+//line.validateSignature(req.body,lineConfig.line.channelSecret);
 
     const client = new line.Client(lineConfig.line);
     Promise.all(req.body.events.map(event => {
@@ -21,8 +26,10 @@ router.post("/webhook", line.middleware(lineConfig.line), async (req: Request, r
             type: 'text',
             text: event.message.text
         });
-    }))
-        .then(result => res.json({ message: "webhook", result: result }));
+    })).then(result => res.json({ message: "webhook", result: result })).catch((err) => {
+            console.error(err);
+            res.status(500).end();
+        });
 
 
 
